62e2cd55e2e4137bafcdfd05ec196de9cfb451be
wanda <wandahalimatu91@gmail.com>
2023-10-06 09:21:33 +0700

    study_kasus_mvc

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASE_URL', 'http://localhost/studykasus/public');
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'db_study');
+?>
---
+++ app/init.php
@@ -0,0 +1,7 @@
+<?php
+require_once 'core/App.php';
+require_once 'core/controller.php';
+require_once 'config/Config.php';
+
+$app = new App();
+?>
